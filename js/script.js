64519b197969f7321100fa68f91890d038fe980b
Alex Basov <alex.basov@cygnati.com>
2019-10-29 20:31:45 +0200

    connect the script, add words array, choose the word randomly

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// set the words array
+let words = [
+  'minute',
+  'ocean',
+  'destruction',
+  'position',
+  'dinosaurs',
+  'crayon',
+  'sticks',
+  'carpenter',
+  'riddle',
+  'cart',
+  'crate',
+  'rifle',
+  'payment',
+  'measure',
+  'purpose',
+  'humor',
+  'food',
+  'run',
+  'loaf',
+  'snails',
+  'berry',
+  'profit',
+  'cup',
+  'winter',
+  'linen',
+  'street',
+  'drain',
+  'kiss',
+  'insect',
+  'toy',
+  'rabbits',
+  'rail',
+  'insurance',
+  'grandmother',
+  'jeans',
+  'art',
+  'cast',
+  'example',
+  'seat',
+  'wilderness',
+  'cloth',
+  'end',
+  'blade',
+  'copper',
+  'growth',
+  'home',
+  'spring',
+  'coat',
+  'sea',
+];
+
+const wordOutput = document.querySelector('#the-word');
+
+// choose random word;
+let word = words[Math.floor(Math.random() * words.length)];
+
+console.log(word);
+wordOutput.innerHTML = word;
+
+
