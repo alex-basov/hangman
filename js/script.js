3d049dcd37776340becfe764a5114c3e82f4900c
Alex Basov <bolex.ua@gmail.com>
2020-03-10 09:14:01 +0200

    refactor the app functions

--- js/script.js
+++ js/script.js
@@ -64,53 +64,41 @@ const submitBtn = document.querySelector('#submit-btn');
 const startGameBtn = document.querySelector('#start-game');
 const form = document.querySelector('form');
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
 // Game variables
 let gameStarted = false;
 let gameOver = false;
 let playerWon = false;
-let wordToGuess = words[Math.floor(Math.random() * words.length)];
-console.log(wordToGuess);
+let word;
+let answerArray = [];
+let remainingLetters;
 let wrongLetters = [];
 let maxWrongLetters = 10;
 let wrongLettersCount = 0;
 
-letterInput.focus();
-hideElement(startGameBtn);
+gameInit();
 
-form.addEventListener("submit", (e) => {
- e.preventDefault();
-});
-
-// answer array
-let answerArray = [];
-wrongLettersArray = [];
-for (let i = 0; i < wordToGuess.length; i++) {
-  answerArray[i] = '_';
-}
-
-let remainingLetters = wordToGuess.length;
-remainingLettersOutput.textContent = remainingLetters.toString();
-wordOutput.textContent = answerArray.join(' ');
-messageOutput.textContent = 'Guess a letter';
-
-submitBtn.onclick = () => {
+submitBtn.addEventListener('click', () => {
   let guess = letterInput.value.toLowerCase().trim();
   guess = validateGuess(guess);
   console.log('Guess:' + guess);
   if (!guess) return;
 
-  if(wordToGuess.indexOf(guess) === -1 && wrongLetters.indexOf(guess) === -1)  {
+  if(word.indexOf(guess) === -1 && wrongLetters.indexOf(guess) === -1)  {
     wrongLetters.push(guess);
     wrongLettersCount = wrongLetters.length;
     wrongAnswersCountOutput.textContent = wrongLettersCount.toString();
     console.log('wrong letters: ' + wrongLetters);
-    messageOutput.textContent = `Wrong letter! you've got ${maxWrongLetters - wrongLettersCount} more tries`;
+    showMessage(`Wrong letter! you've got ${maxWrongLetters - wrongLettersCount} more tries`);
   }
 
-  for (let i = 0; i < wordToGuess.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     if (guess === answerArray[i]) {
       break;
-    } else if (wordToGuess[i] === guess) {
+    } else if (word[i] === guess) {
       answerArray[i] = guess;
       wordOutput.textContent = answerArray.join(' ');
       remainingLetters--;
@@ -121,36 +109,75 @@ submitBtn.onclick = () => {
   wrongLettersOutput.textContent = wrongLetters.join(' | ');
   clearInputValues();
   console.log(remainingLetters);
-  checkMaxWrongAnswers(wrongLettersCount);
-};
+  checkIfLost(wrongLettersCount);
+  letterInput.focus();
+});
+
+
+function gameInit() {
+  hideElement(startGameBtn);
+  word = getRandomWord(words);
+  showLetterPlaceholders();
+  remainingLetters = word.length;
+  showTheRemainingLettersToGuess(remainingLetters);
+  letterInput.focus();
+  messageOutput.textContent = 'Guess a letter';
+}
+
+function getRandomWord(wordsArray){
+  return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+}
+
+function showLetterPlaceholders() {
+  console.log(word);
+  for (let i = 0; i < word.length; i++) {
+    answerArray[i] = '_';
+  }
+  wordOutput.textContent = answerArray.join(' ');
+}
+
+function showTheRemainingLettersToGuess(number) {
+  remainingLettersOutput.textContent = number.toString();
+}
+
+function showMessage(text) {
+  messageOutput.textContent = text;
+}
+
+function hideElement(...elements) {
+  elements.forEach(element => (element.style.display = 'none'));
+}
+
+function showElement(...elements) {
+  elements.forEach(element => (element.style.display = 'unset'));
+}
 
 function validateGuess(guess) {
   if (guess === '') {
-    messageOutput.textContent = 'Please enter something';
+    showMessage('Please enter something');
     return false;
   } else if (guess.length !== 1) {
-    messageOutput.textContent = 'Enter only one letter, please';
+    showMessage('Enter only one letter, please');
     clearInputValues();
     return false;
-  } else{
+  } else {
     return guess;
   }
 }
 
 function checkRemainingLetters(remainingLetters) {
   if (remainingLetters <= 0) {
-    messageOutput.textContent = 'You have won!';
+    showMessage('You have won!');
   }
 }
 
 function clearInputValues() {
   letterInput.value = '';
-  letterInput.focus();
 }
 
-function checkMaxWrongAnswers(number) {
+function checkIfLost(number) {
   if (number >= maxWrongLetters) {
-    messageOutput.textContent = `Too many tries... You've lost`;
+    showMessage(`Too many tries... You've lost`);
     gameOver = true;
     endGame()
   }
@@ -160,12 +187,4 @@ function endGame() {
   showElement(startGameBtn);
   letterInput.disabled = true;
   submitBtn.disabled = true;
-}
-
-function hideElement(...elements) {
-  elements.forEach(element => (element.style.display = 'none'));
-}
-
-function showElement(...elements) {
-  elements.forEach(element => (element.style.display = 'unset'));
 }
